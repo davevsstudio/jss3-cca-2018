b6f8e6c1f1a52fc5d87e8bf5f3219e42730bbe9c
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 22:13:58 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,302 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Art is",
+    answer: "creative expression",
+    options: [
+      "ability to paint",
+      "ability to perform",
+      "ability to carve creativity",
+      "creative expression"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Fine art comprises",
+    answer: "",
+    options: [
+      "drawing, painting and carving",
+      "drawing painting and culpture",
+      "painting, textile and drawing",
+      "texting, painting and ceramic"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The rainbow colours are usually abbreviated as",
+    answer: "ROYGBIV",
+    options: [
+      "ROYVIBG",
+      "ROVGIBY",
+      "ROYGBIB",
+      "ROYGBIV"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A gallery is a place where",
+    answer: "works of arts are displayed for sale",
+    options: [
+      "works of arts are displayed for sale",
+      "works of arts are garnished",
+      "people converge for meeting",
+      "artist works"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Basic shapes includes one of the following",
+    answer: "circle",
+    options: [
+      "scroll",
+      "zig-zag",
+      "sprial",
+      "circle"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of these colours is a complementary of green?",
+    answer: "red",
+    options: [
+      "red",
+      "yellow",
+      "blue",
+      "black"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "The components of Cultural and Creative Arts include the following EXCEPT",
+    answer: "science",
+    options: [
+      "drama",
+      "music",
+      "science",
+      "dance"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Colours placed side by side on the colour wheel are called",
+    answer: "anaglogous colours",
+    options: [
+      "neutral colour",
+      "warm colour",
+      "anaglogous colours",
+      "friendly colours"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Another name for beauty in art is",
+    answer: "aesthetic",
+    options: [
+      "feeling",
+      "beautification",
+      "reality",
+      "aesthetic"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Applied art includes the following designs EXCEPT",
+    answer: "painting",
+    options: [
+      "ceramics design",
+      "painting",
+      "textile design",
+      "graphic design"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Which of these is not a type of line??",
+    answer: "purchasing department",
+    options: [
+      "point",
+      "spriral",
+      "horizontal",
+      "verticial"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Chiaroscuro in art means",
+    answer: "light and shade",
+    options: [
+      "black and shade",
+      "light and shade",
+      "smooth and rough",
+      "dark and cool"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "The person that directs orchestra on choir in the church is called ",
+    answer: "conductor",
+    options: [
+      "conductor",
+      "director",
+      "captain",
+      "manager"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Which of the following colours is common to purple and green?",
+    answer: "red",
+    options: [
+      "yellow",
+      "red",
+      "green",
+      "blue"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Musical note consist if",
+    answer: "7",
+    options: [
+      "9",
+      "8",
+      "7",
+      "6"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Which of these materials is one colour?",
+    answer: "charcoal",
+    options: [
+      "chalk",
+      "crayon",
+      "charcoal",
+      "pastel"
+    ]
+  },
+
+
+  {
+    numb: 17,
+    question: "An artiste means a",
+    answer: "musician",
+    options: [
+      "musician",
+      "graphic artist",
+      "textile designer",
+      "painter"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "An imprrovised brush can be made from",
+    answer: "quill feather",
+    options: [
+      "stick",
+      "wood",
+      "quill feather",
+      "fish tail"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Which of these sets of colours are primary colour",
+    answer: "red, yellow and blue",
+    options: [
+      "red, blue and black",
+      "red, yellow and blue",
+      "red, yellow and orange",
+      "red, blue and green"
+    ]
+  },
+
+
+
+  {
+    numb: 20,
+    question: "Texture refers to the",
+    answer: "quality of the surface of an artwork",
+    options: [
+      "gradition of tones in a drawing",
+      "quality of the surface of an artwork",
+      "arrangement of froms in an artwork",
+      "three dimensional illsioun in a drawing"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "Which of these requires the use of fixative",
+    answer: "chalk pastel",
+    options: [
+      "poster colour",
+      "crayon",
+      "oil painting",
+      "chalk pastel"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "One of the following is NOT an artist's tool",
+    answer: "paint",
+    options: [
+      "palette",
+      "brush",
+      "chisel",
+      "paint"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "An abstract art art is reffered to as",
+    answer: "non-figurative art",
+    options: [
+      "figurative art",
+      "imaginative art",
+      "non-figurative art",
+      "natural art"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Which of the following does not belong to the group?",
+    answer: "balance",
+    options: [
+      "shape",
+      "line",
+      "balance",
+      "texture"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "The arrangement of different shapes or froms in a reapting order is called",
+    answer: "despatch office",
+    options: [
+      "ornametation",
+      "pattern",
+      "design",
+      "decoration"
+    ]
+  },
+];
